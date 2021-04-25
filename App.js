34c98834d22cb9e2ab5c05b44a37bc2af08fcfdf
bodiavaleur <bodiavaleur@github.io>
2021-04-25 16:32:43 +0300

    Added screens and header

--- App.js
+++ App.js
@@ -2,38 +2,44 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {StatusBar, useColorScheme} from 'react-native';
+import {theme} from './theme';
 import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  CategoryDishes,
+  HomePage,
+  SearchPage,
+  DishDetailsPage,
+} from './components/pages/';
+import {Logo, SearchButton} from './components/atoms/';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={RootContainer} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({});
-
-function RootContainer() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={styles.header}>
-        <Text>Hello</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={theme.white}
+      />
+
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerRightContainerStyle: {paddingRight: 10},
+            headerTitle: () => <Logo />,
+            headerTitleAlign: 'center',
+            headerRight: SearchButton,
+            headerBackTitleVisible: false,
+          }}>
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen name="Search" component={SearchPage} />
+          <Stack.Screen name="DishDetails" component={DishDetailsPage} />
+          <Stack.Screen name="CategoryDishes" component={CategoryDishes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
