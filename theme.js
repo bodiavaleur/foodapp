9c0c5f9145d2592114548096d195a1b6c43a9b7b
bodiavaleur <bodiavaleur@github.io>
2021-04-25 16:32:53 +0300

    Added theme

---
+++ theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  primary: 'rgb(90,200,250)',
+  primaryTranslucent: 'rgba(90,200,250, 0.80)',
+  secondary: 'rgb(52,199,89)',
+  text: '#333333',
+  textGray: '#9c9c9c',
+  white: '#ffffff',
+};
